3cf9037492c6fd2ed2d690aa3e7168070157b08d
yang112196 <32599960+yang112196@users.noreply.github.com>
2018-10-17 01:32:03 -0500

    Add files via upload

---
+++ Assets/_Completed-Game/Scripts/CameraController2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController2 : MonoBehaviour {
+    private Vector3 offset;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        offset = this.transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/_Completed-Game/Scripts/PlayerController2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float Hori = Input.GetAxis("Horizontal");
+
+        float Vert = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(Hori, 0.0f, Vert);
+
+        rb.AddForce(movement*speed);
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}

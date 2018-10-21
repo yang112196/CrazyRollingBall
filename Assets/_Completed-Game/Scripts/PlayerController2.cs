9bdd323ba1c7a3a154a36e93a59a9d3d7683e65c
yang112196 <yang112196@gmail.com>
2018-10-21 18:56:44 -0500

    TurtorialCrazyRollingBall

--- Assets/_Completed-Game/Scripts/PlayerController2.cs
+++ Assets/_Completed-Game/Scripts/PlayerController2.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController2 : MonoBehaviour {
     private Rigidbody rb;
+    private int count;
     public float speed;
+    public Text countText;
+    public Text winText;
+
 	// Use this for initialization
 	void Start () {
 
         rb = GetComponent<Rigidbody>();
+
+        count = 0;
+
+        SelectText();
+
+        winText.text = "";
 
 	}
 	
@@ -30,6 +41,16 @@ public class PlayerController2 : MonoBehaviour {
         if(other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count += 1;
+            SelectText();
+        }
+    }
+
+    void SelectText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 8){
+            winText.text = "You win";
         }
     }
 }
